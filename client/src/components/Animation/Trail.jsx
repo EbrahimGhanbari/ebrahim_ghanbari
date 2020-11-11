5d5c3adb36b9b8c28ee3a0330499407a21bb97fe
ghanbari <ghanbari@ualberta.ca>
2020-11-11 03:19:02 -0700

    fix(release): description done

---
+++ client/src/components/Animation/Trail.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useTrail, a } from "react-spring";
+
+function Trail({ open, children, xValue, ...props }) {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 100, tension: 2000, friction: 1000 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: xValue, height: 0 },
+  });
+  return (
+    <div {...props}>
+      <div>
+        {trail.map(({ x, height, ...rest }, index) => (
+          <a.div
+            key={items[index]}
+            style={{
+              ...rest,
+              transform: x.interpolate((x) => `translate3d(0,${x}px,0)`),
+            }}
+          >
+            <a.div style={{ height }}>{items[index]}</a.div>
+          </a.div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Trail;
