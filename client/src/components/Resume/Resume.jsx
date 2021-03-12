306bb72caba67b8c0001ec846991f227897c91cc
ghanbari <ghanbari@ualberta.ca>
2021-03-12 11:47:31 -0700

    resume styling

--- client/src/components/Resume/Resume.jsx
+++ client/src/components/Resume/Resume.jsx
@@ -1,14 +1,68 @@
 import React from "react";
-// import Trail from "../Animation/Trail";
-
 import "./Resume.scss";
 
 export default function (props) {
   return (
     <div className="resume">
       <h1>Resume</h1>
-      <div className="resume-left"></div>
-      <div className="resume-right"></div>
+      <section>
+        <div className="resume-left">
+          <div>
+            <h2>Work Experience</h2>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+          </div>
+          <div>
+            <h2>Skills</h2>
+            <div className="two-column">
+              <dl>
+                <dt>Social Media </dt>
+                <dt>August 2016</dt>
+              </dl>
+              <dl>
+                <dt>Social Media</dt>
+                <dt>August 2016</dt>
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div className="resume-right">
+          <div>
+            <h2>Work Experience</h2>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+          </div>
+          <div>
+            <h2>Volunteering</h2>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+            <h3>Empowered Pathways (WERC)</h3>
+            <dl>
+              <dt>Social Media Strategist (Internship)</dt>
+              <dt>August 2016 – December 2016</dt>
+            </dl>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
