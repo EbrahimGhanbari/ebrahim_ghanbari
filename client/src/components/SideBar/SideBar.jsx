c3377417559c60916854d9fac3aad89c28eba556
ghanbari <ghanbari@ualberta.ca>
2021-03-14 17:08:20 -0600

    side bar created

---
+++ client/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import TrailText from "../Animation/TrailText";
+import Trail from "../Animation/Trail";
+
+import "./TopNavBar.scss";
+
+export default function (props) {
+  const [toggleStatus, setToggleStatus] = useState(true);
+  const [open, set] = useState(true);
+
+  return (
+    <Trail open={open} xValue={-500}>
+      <div className="top_nav">
+        <div className="top_nav_left">
+          <div className="logo">
+            <div
+              onMouseEnter={() => setToggleStatus(false)}
+              onMouseLeave={() => setToggleStatus(true)}
+            >
+              {"E"}
+              <TrailText text={"brahim Ghanbari"} toggle={toggleStatus} />
+            </div>
+          </div>
+        </div>
+        <div className="menu">
+          <ul>
+            <li class="one">
+              <a href="#">Who I am</a>
+            </li>
+            <li class="two">
+              <a href="#"> Résumé</a>
+            </li>
+            <li class="three">
+              <a href="#">Project</a>
+            </li>
+            <li class="four">
+              <a href="#">About</a>
+            </li>
+            <li class="five">
+              <a href="#">Contact</a>
+            </li>
+            <hr />
+          </ul>
+        </div>
+      </div>
+    </Trail>
+  );
+}
