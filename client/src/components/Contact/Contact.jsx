dea5e83dd76fe4004b5a58fb6e62118c9750e4b4
ghanbari <ghanbari@ualberta.ca>
2021-03-15 11:14:59 -0600

    email nodemailer fixed

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import handleViewport from "react-in-viewport";
 import Divider from "../Utilities/Divider";
+import axios from "axios";
 import "./Contact.scss";
 
 const ViewportBlock = handleViewport(Divider /** options: {}, config: {} **/);
 
 export default function (props) {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  function onNameChange(event) {
+    setContact({ ...contact, name: event.target.value });
+  }
+
+  function onEmailChange(event) {
+    setContact({ ...contact, email: event.target.value });
+  }
+
+  function onMessageChange(event) {
+    setContact({ ...contact, message: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios({
+      method: "POST",
+      url: "http://localhost:8080/contact",
+      data: contact,
+    }).then((response) => {
+      if (response.data.status === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  }
   return (
     <div id="contact" className="contact">
       <div>
-        {/* <div style={{ height: "100vh" }}></div> */}
         <ViewportBlock
           height={"0.01vh"}
           onEnterViewport={() => console.log("COntact enter")}
@@ -18,6 +52,44 @@ export default function (props) {
       </div>
       <h1>Contact me</h1>
       <h2>Want to work together? Drop me a line: ghanbari@ualberta.ca</h2>
+      <form id="contact-form" onSubmit={handleSubmit.bind(this)} method="POST">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" onChange={onNameChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={onEmailChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            rows="5"
+            // value={state.message}
+            onChange={onMessageChange.bind(this)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
+    // <div id="contact" className="contact">
+    //   <div>
+    //     {/* <div style={{ height: "100vh" }}></div> */}
+    //     <ViewportBlock
+    //       height={"0.01vh"}
+    //       onEnterViewport={() => console.log("COntact enter")}
+    //       onLeaveViewport={() => console.log("COntact leave")}
+    //     />
+    //   </div>
+    //   <h1>Contact me</h1>
+    //   <h2>Want to work together? Drop me a line: ghanbari@ualberta.ca</h2>
+    // </div>
   );
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const BodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const path = require("path");
-// require('dotenv').config();
+require('dotenv').config();
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -17,23 +17,20 @@ App.use(Logger("dev"));
 const whitelist = [
   "http://localhost:3000",
   "http://localhost:8080",
-  // "https://cnergreen.herokuapp.com",
 ];
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
+    // console.log("** Origin of request " + origin);
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
+      // console.log("Origin acceptable");
       callback(null, true);
     } else {
-      console.log("Origin rejected");
+      // console.log("Origin rejected");
       callback(new Error("Not allowed by CORS"));
     }
   },
 };
 App.use(cors(corsOptions));
-
-
 
 App.use(
   cors({
@@ -42,60 +39,48 @@ App.use(
   })
 );
 
-// Sample GET route
-App.get("/api/data", (req, res) => {
+App.get("/contact", (req, res) => {
   res.json({
     message: "Seems to work yay!",
   });
 });
 
-// This section handle sending emails
-// Refer to this video for more instructions
-// https://www.youtube.com/watch?v=zEKeNOo9woo&ab_channel=Vuka
-// App.post("/message", async (req, res) => {
-//   console.log(req.body);
-//   const email = req.body;
-//   const plainTextMsg = `
-//     Name: ${email.name} ** 
-//     Email: ${email.email} ** 
-//     Content: ${email.content}`;
-//   const htmlTextMsg = `
-//   <h3> Name: </h3> <p> ${email.name} </p>
-//   <h3> Email: </h3> <p> ${email.email} </p>
-//   <h3> Content: </h3> <p> ${email.content} </p>`;
+App.post("/contact", async (req, res) => {
+  const email = req.body;
+  const plainTextMsg = `
+  Name: ${email.name} ** 
+  Email: ${email.email} ** 
+  Content: ${email.message}`;
+  const htmlTextMsg = `
+  <h3> Name: </h3> <p> ${email.name} </p>
+  <h3> Email: </h3> <p> ${email.email} </p>
+  <h3> Content: </h3> <p> ${email.message} </p>`;
 
-//   // create reusable transporter object using the default SMTP transport
-//   let transporter = nodemailer.createTransport({
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: "erin.boyle15@ethereal.email", // generated ethereal user
-//       pass: "Fr146xEM8fKV7sJXTf", // generated ethereal password
-//     },
-//   });
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASSWORD, // generated ethereal password
+    },
+  });
 
-//   const msg = {
-//     from: '"The Express App" <theExpressApp@example.com>', // sender address
-//     to: "erin.boyle15@ethereal.email", // list of receivers
-//     subject: "Rquest for info - Website", // Subject line
-//     text: plainTextMsg, // plain text body
-//     html: htmlTextMsg, // html body
-//   };
+  const msg = {
+    from: 'pet.ghanbari@gmail.com', // sender address
+    to: "pet.ghanbari@gmail.com", // list of receivers
+    subject: "Rquest for info - Website",
+    text: plainTextMsg,
+    html: htmlTextMsg,
+  };
 
-//   let info = await transporter.sendMail(msg);
+  let info = await transporter.sendMail(msg);
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-//   console.log("Message sent: %s", info.messageId);
-//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  res.send("Email sent!");
+});
 
-//   // Preview only available when sending through an Ethereal account
-//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-//   res.send("Email sent!");
-// });
-
-// Reading built file in production mode
 if (process.env.NODE_ENV === "production") {
   App.use(Express.static(path.join(__dirname, "client/build")));
 }
