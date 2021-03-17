16436916f6ada05930703861613d4a1150081ef8
ghanbari <ghanbari@ualberta.ca>
2021-03-17 09:09:31 -0600

    fixed project

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import handleViewport from "react-in-viewport";
 import Divider from "../Utilities/Divider";
 import "./Projects.scss";
@@ -7,19 +7,26 @@ const ViewportBlock = handleViewport(Divider /** options: {}, config: {} **/);
 
 export default function (props) {
   return (
-    <div id="project" className="project">
+    <div>
       <div>
-        {/* <div style={{ height: "100vh" }}></div> */}
         <ViewportBlock
           height={"0.01vh"}
           onEnterViewport={() => console.log("project enter")}
           onLeaveViewport={() => console.log("project leave")}
         />
       </div>
-      <h1>Selected Works</h1>
-      <div>
-        <div className="project-1"></div>
-        <div className="project-2"></div>
+      <div id="project">
+        <h1>Selected Works</h1>
+        <div className="project">
+          <div className="box project_1">
+            <img src="cnergreen.png" alt="Italian Trulli" />
+            <h2 className="box_text">TEXT TEXT</h2>
+          </div>
+          <div className="box project_2">
+            <img src="cnergreen.png" alt="Italian Trulli" />
+            <h2 className="box_text">TEXT TEXT</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
