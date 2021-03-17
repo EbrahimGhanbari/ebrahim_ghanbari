9b99ad44fa8b656d2d77486b1742969b6133db3d
ghanbari <ghanbari@ualberta.ca>
2021-03-17 11:05:53 -0600

    card added

---
+++ client/src/components/Animation/Card.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import "./Card.scss";
+
+export default function () {
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  return (
+    <div onClick={() => set((state) => !state)}>
+      <animated.div
+        className="c back"
+        style={{ opacity: opacity.interpolate((o) => 1 - o), transform }}
+      />
+      <animated.div
+        className="c front"
+        style={{
+          opacity,
+          transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
+        }}
+      />
+    </div>
+  );
+}
