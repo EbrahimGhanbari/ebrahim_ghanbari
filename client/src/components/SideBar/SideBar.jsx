5a8812d1ec1d25e7c63bf43259f829421f7b6c78
ghanbari <ghanbari@ualberta.ca>
2021-03-18 10:58:06 -0600

    fixed margin

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -8,40 +8,40 @@ export default function (props) {
     <div className="sidebar">
       <div>
         <Link
-          className="link-pionter"
+          className="link-pointer"
           activeClass="active"
           to="project"
           spy={true}
           smooth={true}
         >
-          Project
+          <div className=""> Project</div>
         </Link>
         <Link
-          className="link-pionter"
+          className="link-pointer"
           activeClass="active"
           to="resume"
           spy={true}
           smooth={true}
         >
-          Résumé
+          <div className=""> Résumé</div>
         </Link>
         <Link
-          className="link-pionter"
+          className="link-pointer"
           activeClass="active"
           to="about"
           spy={true}
           smooth={true}
         >
-          About
+          <p className=""> About</p>
         </Link>
         <Link
-          className="link-pionter"
+          className="link-pointer"
           activeClass="active"
           to="contact"
           spy={true}
           smooth={true}
         >
-          Contact
+          <p className="sidebar_border"> Contact</p>{" "}
         </Link>
       </div>
     </div>
