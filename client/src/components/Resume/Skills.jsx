8f7476416e7c1eb0b7654414e834f3ece0dc810a
ghanbari <ghanbari@ualberta.ca>
2021-07-10 12:15:55 -0600

    cv updated

--- client/src/components/Resume/Skills.jsx
+++ client/src/components/Resume/Skills.jsx
@@ -8,21 +8,41 @@ export default function () {
       <h2 className="resume_subtitle">Skills</h2>
       <div className="skills">
         <dl>
-          <dt>Javascript </dt>
+          <dt>Javascript</dt>
           <dt>ReactJS </dt>
-          <dt>Python</dt>
+          <dt>Javascript </dt>
           <dt>Node JS </dt>
-          <dt>NOSQL/SQL </dt>
-          <dt>Machine Learning</dt>
+
+          <dt>Test Driven Development </dt>
+          <dt>NOSQL: Firebase </dt>
         </dl>
         <dl>
-          <dt>Apache</dt>
+          <dt>Ruby on Rails</dt>
           <dt>Sass</dt>
           <dt>Bootstrap</dt>
-          <dt>Test-Driven Development </dt>
-          <dt>Ruby on Rails</dt>
+          <dt>Python</dt>
+          <dt>SQL: Postgres, MySQL</dt>
         </dl>
       </div>
+      {/* <div className="skills">
+        <dl>
+          <dt>Javascript</dt>
+          <dt>ReactJS </dt>
+          <dt>Javascript </dt>
+          <dt>Node JS </dt>
+        </dl>
+        <dl>
+          <dt>Test Driven Development </dt>
+          <dt>NOSQL: Firebase </dt>
+        </dl>
+        <dl>
+          <dt>Ruby on Rails</dt>
+          <dt>Sass</dt>
+          <dt>Bootstrap</dt>
+          <dt>Python</dt>
+          <dt>SQL: Postgres, MySQL</dt>
+        </dl>
+      </div> */}
     </div>
   );
 }
