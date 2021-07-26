e34ad8fef0540293764826cda20dabd25f87e712
ghanbari <ghanbari@ualberta.ca>
2021-07-25 20:13:41 -0600

    updated resume

--- client/src/components/Resume/Educations.jsx
+++ client/src/components/Resume/Educations.jsx
@@ -4,21 +4,21 @@ export default function () {
   return (
     <div>
       <h2 className="resume_subtitle">Education</h2>
-      <h2>Diploma • Web Development</h2>
-      <dl>
-        <dt>Lighthouse Labs</dt>
-        <dt>2020</dt>
-      </dl>
       <h2>Msc. • Petroleum Engineering</h2>
       <dl>
         <dt>University of Alberta</dt>
         <dt> 2012 - 2014</dt>
       </dl>
-      {/* <h2>Bsc. • Petroleum Engineering</h2>
-                <dl>
-                  <dt>Amirkabir University of Technology</dt>
-                  <dt> 2007 - 2011</dt>
-                </dl> */}
+      <h2>Bsc. • Computer Science</h2>
+      <dl>
+        <dt>Amirkabir University of Technology</dt>
+        <dt> 2010 - 2012</dt>
+      </dl>
+      <h2>Bsc. • Petroleum Engineering</h2>
+      <dl>
+        <dt>Amirkabir University of Technology</dt>
+        <dt> 2006 - 2010</dt>
+      </dl>
     </div>
   );
 }
--- client/src/components/Resume/WorkExperience.jsx
+++ client/src/components/Resume/WorkExperience.jsx
@@ -5,7 +5,7 @@ export default function () {
     <div>
       <h2 className="resume_subtitle">Work Experience</h2>
 
-      <h2>Application Specialist - Completion & Liner System</h2>
+      {/* <h2>Application Specialist - Completion & Liner System</h2>
       <dl>
         <dt>Weatherford Canada Ltd.</dt>
         <dt>December 2017 - Present</dt>
@@ -21,26 +21,25 @@ export default function () {
       <dl>
         <dt>Weatherford Canada Ltd.</dt>
         <dt>October 2014 - October 2016</dt>
-      </dl>
+      </dl> */}
 
-      {/* <h2>Software Developer </h2>
+      <h2>Software Developer </h2>
       <dl>
         <dt>Weatherford Canada Ltd.</dt>
         <dt>September 2019 - Present</dt>
-      </dl> 
+      </dl>
 
       <h2>Application Specialist </h2>
       <dl>
         <dt>Weatherford Canada Ltd.</dt>
         <dt>October 2016 - September 2019</dt>
       </dl>
-      
-      
-    <h2>Application Specialist - Data Analyst </h2>
+
+      <h2>Application Specialist - Data Analyst </h2>
       <dl>
         <dt>Weatherford Canada Ltd.</dt>
         <dt>October 2014 - October 2016</dt>
-      </dl> */}
+      </dl>
       <h2>Web Development Mentor • Part Time</h2>
       <dl>
         <dt>Lighthouse Labs</dt>
